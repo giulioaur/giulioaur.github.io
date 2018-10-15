6d80811118d1b6e60771c9c3af02101eb3c547a0
giulioaur <giulioaur@gmail.com>
2018-10-15 23:57:39 +0200

    First version of interactive CV

---
+++ Scripts/cv.js
@@ -0,0 +1,372 @@
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// GLOBAL /////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+var input_map = { '73' : () => { parser.stopPreviousExecution(); toggle_talents_tree(true); }};
+var global = { acceptInput : true }
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// GENERAL ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Inits all components.
+ */
+function init() {
+    // Hide sections.
+    $('#character').hide();
+    $('#character_code_environment').hide();
+
+    // Create pgs for selection.
+    create_pg();
+    parser.init();
+
+    // set_character(0);
+
+    // // parser.writeString(`int main(){
+    // //                             int x = 2;
+    // //                             printf("%d", x);
+    // //                         }`, $('#test_span'))
+    // $('#character_talents').hide();
+    // parser.parse(pgs.list[0].abilities[0].code);
+}
+
+/**
+ * Called on resize window.
+ */
+function resize(){
+    // Aestetic stuffs.
+    move_fake_border();
+
+    // Re-position the equip div.
+    position_equip();
+
+    // Re-position the fake inventory.
+    position_fake_inventory();
+
+    // Resize talents tabs.
+    resize_talents_categories()
+}
+
+/**
+ * Handles the input.
+ * 
+ * @param {Event} event The event.
+ */
+function handle_input(event){
+    if (global.acceptInput && input_map.hasOwnProperty(event.keyCode)){
+        // Execute ability.
+        global.acceptInput = false;
+        input_map[event.keyCode]();
+        $('#ability_' + event.keyCode).addClass('activated');
+
+        // Let ability border blink.
+        setTimeout(() => {
+            $('#ability_' + event.keyCode).removeClass('activated');
+            $('#character_abilities .loading_screen').addClass('loading');
+
+            // Make other abilities usable after loading is finished.
+            setTimeout(() => {
+                $('#character_abilities .loading_screen').removeClass('loading');
+                global.acceptInput = true;
+            }, 1000);
+        }, 100)
+        
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// SELECTION ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Creates new character for the selection.
+ */
+function create_pg(){
+    let $selection = $('#selection');
+
+    // For each pg create a row.
+    pgs.list.forEach((pg, i) => {
+        let newCharacter = domer.createPg(pg.avatar, pg.name, pg. race, pg.spec, pg.background);
+        newCharacter.onclick = () => {choose_character(i)};
+        newCharacter.id = 'pg_' + i;
+        $selection.append($(newCharacter));
+    });
+
+    // Show a background.
+    $('.fake_body').eq(0).show();
+}
+
+/**
+ * Choose a character to show.
+ * 
+ * @param {number} idx The index of the character in the characters array.
+ */
+async function choose_character(idx){
+    // Start rendering pg in background.
+    var promise = set_character(idx);
+
+    // Move avatar.
+    let $old = $('#pg_' + idx + ' img');
+
+    // First we copy the image a couple of time.
+    let $new = $old.clone().appendTo('#character_avatar');
+    let $temp = $old.clone().appendTo('body').css('border', '6px solid rgba(0,0,0,0)');
+
+    // Store attributes.
+    let newOffset = $new.offset(), newDim = $new.css('width');
+    let oldOffset = $old.offset();
+
+    // hide new and old and move $temp to position
+    // also big z-index, make sure to edit this to something that works with the page
+    $temp.css({ 'position': 'absolute', 'left': oldOffset.left, 'top': oldOffset.top, 'zIndex': 1000 });
+    $old.hide();
+
+    // Start fade and movement animations.
+    $('#selection').hide(400);
+    $temp.animate({ 'top': newOffset.top, 'left': newOffset.left, 'width': newDim }, 'slow', function () {
+        // Wait the pg to be rendered.
+        async_show(promise, $temp);
+    });
+}
+
+/**
+ * Waits for pg to be rendered and then shows it.
+ * 
+ * @param {Promise} promise The function rendering pg in background.
+ * @param {Element} $temp The fake avatar to delete.
+ */
+async function async_show(promise, $temp){
+    // Wait rendering.
+    await promise;
+    
+    // Show it.
+    let $ch = $('#character');
+    $ch.hide();
+    $ch.css('z-index', 600);
+    $('#character').show(700, () => {
+        $temp.remove();
+        create_fake_border();
+    });
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// SHOW //////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+let borderSize = 3,             // Border size around pg.
+    equips,                     // The equips square div.
+    $fakeInventory,             // The inventory div to fake closing and opening.
+    isTogglingInvetory;         // Flag to be sure the inventory cannot be toggled twice a time.
+
+/**
+ * Prepares character visualization in background.
+ * 
+ * @param {number} index The index of the character.
+ */
+function set_character(index){
+    let $ch = $('#character');
+    $ch.show();
+    $ch.css('z-index', '-1');
+    
+    return new Promise( (resolve) => {
+        domer.showPg(pgs.list[index]);
+
+        // Find equips divs and position them. 
+        find_equip();
+        position_equip();
+
+        // Inventory stuffs.
+        create_fake_inventory();
+
+        // Resize talents tabs.
+        resize_talents_categories();
+
+        resolve();
+    });
+}
+
+/**
+ * Creates a fake avatar components to simulate border around avatar and life.
+ */
+function create_fake_border() {
+    // Border for health bar.
+    let $chInfo = $("#character_info > div");
+    let offset = $chInfo.offset();
+    let $fakeCI = $chInfo.clone().appendTo('body').attr('id', 'fake_character_info');
+
+    $fakeCI.css({
+        'position': 'absolute', 'left': offset.left - borderSize, 'top': offset.top - borderSize, 'zIndex': 300,
+        'width': $chInfo.width() + borderSize * 2, 'height': $chInfo.height() + borderSize * 2, 'border': borderSize + 'px ridge gray',
+        'border-radius': '10px'
+    });
+
+    // Border for avatar.
+    let $chImg = $("#character_avatar > img");
+    offset = $chImg.offset();
+    let $fakeCA = $chImg.clone().appendTo('body').attr('id', 'fake_character_avatar');
+    let imgBorder = parseInt($chImg.css("border-left-width"));
+
+    $fakeCA.css({
+        'position': 'absolute', 'left': offset.left - borderSize, 'top': offset.top - borderSize, 'zIndex': 300,
+        'width': $chImg.width() + (borderSize + imgBorder) * 2, 'height': $chImg.height() + (borderSize + imgBorder) * 2, 'border': borderSize + 'px ridge gray',
+        'border-radius': '100%'
+    });
+}
+
+/**
+ * Resizes the fake border.
+ */
+function move_fake_border(){
+    let offset = $("#character_info > div").offset();
+    $('#fake_character_info').css({
+        'left': offset.left - borderSize, 'top': offset.top - borderSize, 
+        'width': $("#character_info > div").width() + borderSize * 2, 'height': $("#character_info > div").height() + borderSize * 2,
+    });
+
+    offset = $("#character_avatar > img").offset();
+    let imgBorder = parseInt($("#character_avatar > img").css("border-left-width"));
+    $('#fake_character_avatar').css({
+        'left': offset.left - borderSize, 'top': offset.top - borderSize,
+        'width': $("#character_avatar > img").width() + (borderSize + imgBorder) * 2, 
+        'height': $("#character_avatar > img").height() + (borderSize + imgBorder) * 2,
+    });
+}
+
+/**
+ * Initializes equip object.
+ */
+function find_equip(){
+    equips = {
+        helmet: { $div: $('#equip_helmet'), right: 50, top: 6 },
+        rightHand: { $div: $('#equip_right_hand'), right: 8, top: 56 },
+        leftHand: { $div: $('#equip_left_hand'), right: 88, top: 56 },
+        body: { $div: $('#equip_body'), right: 49, top: 30 },
+        legs: { $div: $('#equip_legs'), right: 49, top: 70 },
+        foot: { $div: $('#equip_foot'), right: 50, top: 93 }
+    };
+}
+
+/**
+ * Positions the equip div.
+ */
+function position_equip(){
+    let $equipDiv = $('#character_equip > img');
+
+    for (var equip in equips)
+        place_equip(equips[equip], $equipDiv.width(), $equipDiv.height(), $equipDiv.offset());
+}
+
+/**
+ * Sets the equip div to the right position wrt the image.
+ * 
+ * @param {object} equip Equip object.
+ * @param {number} parentWidth Width of the image on which position the equip div.
+ * @param {number} parentHeight Height of the image on which position the equip div.
+ * @param {number} parentOffset Offset of the image on which position the equip div.
+ */
+function place_equip(equip, parentWidth, parentHeight, parentOffset){
+    let x = parentOffset.left + parentWidth / 100.0 * equip.right;
+    let y = parentOffset.top + parentHeight / 100.0 * equip.top;
+
+    equip.$div.offset({ left: x - equip.$div.width() / 2., top: y - equip.$div.height() / 2.});
+}
+
+/**
+ * Creates the fake inventory to simulate the inventory closing and opening.
+ */
+function create_fake_inventory(){
+    let $inventory = $('#inventory_internal');
+    let offset = $inventory.offset();
+
+    $fakeInventory = $('#inventory_internal').clone(true).appendTo('body');
+    $fakeInventory.css({ 'position': 'absolute', 'left': offset.left, 'top': offset.top, 'zIndex': 500,
+                        'width': $inventory.width(), 'height': $inventory.height(), 'border': $inventory.css('border')});
+    $fakeInventory.hide();
+}
+
+/**
+ * Re-position the fake inventory.
+ */
+function position_fake_inventory() {
+    $fakeInventory.show();
+
+    // Re-position and resize fake inventory.
+    if ($('#inventory_internal').is(':hidden')) {
+        $fakeInventory.offset($('#inventory_button > i').offset());
+    }
+    else {
+        $fakeInventory.css({ 'width': $('#inventory_internal').width(), 'height': $('#inventory_internal').height()});
+        $fakeInventory.offset($('#inventory_internal').offset());
+    }
+
+
+    $fakeInventory.hide();
+}
+
+/**
+ * Toggles the visibility of the inventory.
+ */
+function toggle_inventory(){
+    if(!isTogglingInvetory) {
+        isTogglingInvetory = true;
+
+        // Store needed element.
+        let $inventory = $('#inventory_internal');
+        let $button = $('#inventory_button > i');
+        let isHidden = $inventory.is(':hidden');
+
+        // To allow offset correctly works.
+        $inventory.show();
+
+        let finalOffset = isHidden ? $inventory.offset() : $button.offset();
+        let finalWidth = isHidden ? $inventory.width() : $button.width();
+        let finalHeight = isHidden ? $inventory.height() : $button.height();
+
+        // Use the fake inventory for the effect.
+        $fakeInventory.show();
+        $inventory.hide();
+
+        $fakeInventory.animate({ 'top': finalOffset.top, 'left': finalOffset.left, 'width': finalWidth, 'height': finalHeight}, 'slow', () => {
+            $fakeInventory.hide();
+
+            if (isHidden) $inventory.show();
+
+            isTogglingInvetory = false;
+        });
+    }
+}
+
+/**
+ * Resizes the talents category's tab.
+ */
+function resize_talents_categories(){
+    $('.talents_category').height($('#character_talents').height() - $('#character_talents ul').height() - 10);
+
+    $('.talents_category .talent_info > .col-3 > img').resize(() => {console.log("Ok");});
+}
+
+/**
+ * Toggles popus on items.
+ */
+function toggle_popup(){
+    $(event.target).popover('toggle');
+}
+
+/**
+ * Shown the talents or abilities window.
+ * 
+ * @param {boolean} showTalentsWindow True if the talents window has to be shown.
+ */
+function toggle_talents_tree(showTalentsWindow){
+    let $talents = $('#character_talents'), $abilities = $('#character_code_environment');
+
+    // Need to swap visibility.
+    if ($talents.is(':hidden') == showTalentsWindow){
+        if (showTalentsWindow){
+            $abilities.hide(); $talents.show(); 
+        }
+        else{
+            $talents.hide(); $abilities.show();
+        }
+    }
+}
---
+++ Scripts/cvParser.js
@@ -0,0 +1,292 @@
+//////////////////////////////////////// CONSTANTS ////////////////////////////////////////
+const WRITING_SPEED = 20;                           
+const NODE_TYPES = ['NODEC', 'NODET', 'NODED', 'NODEI', 'NODEP'];     // I know that this is not properly constant.
+const EVENT_NAME = "execute";
+const HTML_WORDS = { '\n': '<br>', '\t': '&emsp;', '<': '&lsaquo;', '>': '&rsaquo;'}
+var ELEMENTS;
+
+//////////////////////////////////////// GLOBAL ////////////////////////////////////////
+var parserGlobal = {isExecuting: false, stopExecution: false};
+
+/**
+ * Node to construct the parser list.
+ */
+class Node{
+    /**
+     * Creates a new node.
+     * 
+     * @param {number} idx The ordered position of the node.
+     * @param {string} content The content of the node.
+     * @param {JqueryElement} $father The father element in the HTML page.
+     */
+    constructor(idx, content = '', $father = null, type = 'D'){
+        this._idx = idx;
+        this._type = type;
+        this._content = content;
+        this._element = $father != null ? $('<span id="node_' + idx + '"></span>').appendTo($father) : null;
+    }
+
+    /**
+     * The index of the node.
+     */
+    get idx() { return this._idx; }
+
+    /**
+     * The type of the node.
+     */
+    get type() { return this._type; }
+ 
+    /**
+     * The element containing the node content.
+     */
+    get $element() { return this._element; }
+
+    /**
+     * The content of the node.
+     */
+    get content() { return this._content; }
+    set content(cont) { this._content = cont; }
+}
+
+var parser = {
+
+/**
+ * Init all the stuff needed by the parser.
+ */
+init(){
+    ELEMENTS = Object.freeze({ROOT : document.getElementById('character_code_environment'), $CURSOR : $('#blinking_cursor')});
+    ELEMENTS.ROOT.addEventListener(EVENT_NAME, this.execute);
+},
+
+/**
+ * Parses the text and starts the execution of the obtained code.
+ * 
+ * @param {string} str The string to parse
+ */
+parse(str){
+    // Retrieve list of nodes and sort them by their execution order.
+    let list = this.buildNodeList(str);
+    list.sort((a, b) => { return a.idx - b.idx; });
+
+    // Start node execution.
+    parserGlobal.isExecuting = true;
+    ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: {parser: this, list: list, index : 0} }));
+},
+
+/**
+ * Builds the list of instructions as nodes.
+ * 
+ * @param {string} str The string to parse.
+ * @return {Node[]} The list of instructions.
+ */
+buildNodeList(str){
+    let nodeList = [];
+    let pos = 0;
+
+    // Parse whole string.
+    while (pos < str.length && pos >= 0){
+        if (str[pos] != '\\')
+            throw Error('Expected "\\NODE" at position ' + pos + '.');
+
+        ++pos;
+        let nodeType = str.substr(pos, 5);
+
+        if (NODE_TYPES.includes(nodeType))
+            pos = this.parseNode(str, pos + 5, nodeList, nodeType.substr(4, 1));   
+        else
+            throw Error(`${nodeType} at position ${pos} is not a valid node type.`);
+    }
+
+    return nodeList;
+},
+
+/**
+ * Parse a node.
+ * 
+ * @param {string} str The string to parse.
+ * @param {number} pos The string position.
+ * @param {Node[]} list The list in which insert the node.
+ * @param {string} type The type of the node.
+ * 
+ * @returns {number} The new position in the string.
+ */
+parseNode(str, pos, list, type){
+    // Check for error in syntax.
+    let closure = str.indexOf(']', pos + 1);
+    if (str[pos] != '[' || closure < pos)
+        throw Error('Expected "[/digits/]" at position ' + pos + '.');
+    
+    // Parse index of the node.
+    let index = str.substr(pos + 1, closure - pos - 1);
+    if (Number(index) == NaN)
+        throw Error('Expected a number at position ' + pos + 1 + '.');
+
+    // Check type of node and parse it.
+    closure++;
+    if (type == 'C' || type == 'T' || type == 'I'){        // Text node.
+        let newPos = str.indexOf('\\NODE', closure);
+        let content = str.substr(closure, (newPos > 0 ? newPos : str.length) - closure);
+        list.push(new Node(parseInt(index), content, type == 'C' ? $('#code_paper .node_father') : $('#code_terminal .node_father'), type));
+        return newPos;
+    }
+    else if (type == 'D' || type == 'P'){                  // Deleter and pause node.
+        let closure2 = str.indexOf(']', closure);
+        if (str[closure] != '[' || closure2 < closure)
+            throw Error('Expected "[/list_of_nodes_to_delete/]" at position ' + closure + '.');
+        
+        list.push(new Node(parseInt(index), str.substr(closure + 1, closure2 - closure - 1), null, type));
+        return str.indexOf('\\NODE', closure2 + 1);
+    }
+    else{
+        throw Error('Type \\NODE' + type + 'is not a valid node.');
+    }
+},
+
+/**
+ * Executes a node.
+ * 
+ * @param {{detail: {parser: {}, list: array, index: number}}} container The object containing list of events and index of current node.
+ */
+execute(container){
+    let list = container.detail.list, index = container.detail.index;
+
+    if (!parserGlobal.stopExecution && index < list.length){
+        let type = list[index].type;
+
+        // Call the right execution method.
+        switch(type){
+            case 'C': case 'T': case 'I':
+                container.detail.parser.textNode(list, index); break;
+            case 'D':
+                container.detail.parser.deleterNode(list, index); break;
+            case 'P':
+                container.detail.parser.pauseNode(list, index); break;
+        }
+    }
+    else{
+        parserGlobal.isExecuting = false;
+    }
+},
+
+/**
+ * Executes a text node.
+ * 
+ * @param {Node[]} nodeList The list of nodes.
+ * @param {number} index The index of the current node.
+ */
+textNode(nodeList, index) {
+    let str = nodeList[index].content, $element = nodeList[index].$element;
+    let currentStr = "", currentIdx = 0;
+
+    // Format tabs.
+    str = str.split('    ').join('\t');
+
+    // Move cursor.
+    ELEMENTS.$CURSOR.insertAfter($element);
+
+    if (nodeList[index].type == 'I'){           // Write all text at once.
+        for (var i = 0; i < str.length; i++)
+            currentStr += HTML_WORDS.hasOwnProperty(str[i]) ? HTML_WORDS[str[i]] : str[i];
+
+        $element.html(currentStr);
+        ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { parser: this, list: nodeList, index: index + 1 } }));
+    }
+    else {                                      // Write the text incrementally.
+        let addLetter = setInterval(() => {
+            if (currentIdx < str.length && !parserGlobal.stopExecution) {
+                let ch = str[currentIdx];
+
+                if (HTML_WORDS.hasOwnProperty(ch)) currentStr += HTML_WORDS[ch];           // Break line.          
+                else currentStr += ch;                                                      // Normal letter.
+
+                ++currentIdx;
+                $element.html(currentStr);
+
+                if (nodeList[index].type == 'T'){
+                    $('#code_terminal').scrollTop($('#code_terminal > div').prop("scrollHeight"));
+                }
+            }
+            else {
+                clearInterval(addLetter);
+                ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { parser: this, list: nodeList, index: index + 1 } }));
+            }
+        }, WRITING_SPEED);
+    }
+    
+},
+
+/**
+ * Executes a deleter node.
+ * 
+ * @param {Node[]} nodeList The list of nodes.
+ * @param {number} index The index of the current node.
+ */
+deleterNode(nodeList, index){
+    let toDelete = nodeList[index].content.split(',');
+
+    ELEMENTS.$CURSOR.insertAfter($('#node_' + toDelete[toDelete.length - 1]));
+
+    setTimeout(() => {
+        let $nodes = $(toDelete.map(x => '#node_' + x).join(', '));
+        $nodes.css('background-color', 'gray');
+        ELEMENTS.$CURSOR.insertBefore($nodes.eq(0));
+
+        setTimeout(() => {
+            $nodes.remove();
+            ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { parser: this, list: nodeList, index: index + 1 } }));
+        }, 200)
+    }, 200);
+},
+
+/**
+ * Executes a pause node.
+ * 
+ * @param {Node[]} nodeList The list of nodes.
+ * @param {number} index The index of the current node.
+ */
+pauseNode(nodeList, index){
+    // Set pause.
+    let timeout = setTimeout(() => {
+        ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { parser: this, list: nodeList, index: index + 1 } }));
+        clearInterval(interval);
+    }, parseInt(nodeList[index].content));
+
+    // Check if interruption happens.
+    let interval = setInterval(() => {
+        if (parserGlobal.stopExecution){
+            clearTimeout(timeout);
+            clearInterval(interval);
+            ELEMENTS.ROOT.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { parser: this, list: nodeList, index: index + 1 } }));
+        }
+    }, WRITING_SPEED);
+},
+
+/**
+ * Stop the previous current execution if any.
+ */
+stopPreviousExecution: async () => {
+    if (parserGlobal.isExecuting) {
+        // Start the stop process.
+        let stopper = new Promise((resolve) => {
+            parserGlobal.stopExecution = true;
+
+            // Check until the previous execution is no more running.
+            let interval = setInterval(() => {
+                if (!parserGlobal.isExecuting) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, WRITING_SPEED)
+        });
+
+        await stopper;
+        parserGlobal.stopExecution = false;
+
+        // Clean up all.
+        ELEMENTS.$CURSOR.appendTo(ELEMENTS.ROOT);
+        $('#code_paper .node_father, #code_terminal .node_father').empty();
+        ELEMENTS.$CURSOR.appendTo($('#code_paper .node_father'));
+    }
+}
+
+}
